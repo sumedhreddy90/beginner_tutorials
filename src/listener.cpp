af78384dd31d181723e86be65fae251941b8a3c8
sumedhreddy90 <sumedhreddykoppula@gmail.com>
2021-10-31 22:19:53 -0400

    [Task 7] Creating a new branch- ros_pub_sub and modifying publisher node with custom string message

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,16 +1,15 @@
+// Copyright 2021 Sumedh Reddy Koppula
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
 /**
  * This tutorial demonstrates simple receipt of messages over the ROS system.
  */
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
+  ROS_INFO("Hey you!! Im listening: [%s]", msg->data.c_str());
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
--- src/talker.cpp
+++ src/talker.cpp
@@ -1,3 +1,4 @@
+// Copyright 2021 Sumedh Reddy Koppula
 #include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
@@ -5,8 +6,7 @@
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -52,15 +52,14 @@ int main(int argc, char **argv)
    * a unique string for each message.
    */
   int count = 0;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "I bake Robots" << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
